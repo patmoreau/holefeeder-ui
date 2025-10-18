84fca937891e3af08c81baa93e829e5b4d54c019
Patrick Moreau <patrick@drifterapps.com>
2025-10-18 00:04:24 -0400

    theme CategoryPicker

---
+++ holefeeder-react/src/components/CategoryPicker.tsx
@@ -0,0 +1,44 @@
+import { Picker } from '@react-native-picker/picker';
+import React from 'react';
+import { View } from 'react-native';
+import { Category } from '@/core';
+import { useTextStyles } from '@/hooks';
+import { LoadingIndicator } from './ui';
+
+type Props = {
+  categories: Category[] | null;
+  selectedCategory: Category | null;
+  onSelectCategory: (category: Category) => void;
+};
+
+export function CategoryPicker({ categories, selectedCategory, onSelectCategory }: Props) {
+  const textStyles = useTextStyles();
+
+  if (!categories) {
+    return <LoadingIndicator size="small" />;
+  }
+
+  return (
+    <View
+      style={{
+        position: 'relative',
+        width: '100%',
+      }}
+    >
+      <Picker
+        style={textStyles.picker}
+        selectedValue={selectedCategory?.id}
+        onValueChange={(itemValue: string | number) => {
+          const selected = categories.find((cat) => cat.id === itemValue);
+          if (selected) {
+            onSelectCategory(selected);
+          }
+        }}
+      >
+        {categories.map((category) => (
+          <Picker.Item key={category.id} label={category.name} value={category.id} style={textStyles.pickerItem} />
+        ))}
+      </Picker>
+    </View>
+  );
+}

1ac602d4c06df38036916fcefedbd35ba2420abb
Patrick Moreau <patrick@drifterapps.com>
2025-10-18 00:02:10 -0400

    theme AccountPicker

---
+++ holefeeder-react/src/components/AccountPicker.tsx
@@ -0,0 +1,44 @@
+import { Picker } from '@react-native-picker/picker';
+import React from 'react';
+import { View } from 'react-native';
+import { Account } from '@/core';
+import { useTextStyles } from '@/hooks';
+import { LoadingIndicator } from './ui';
+
+type Props = {
+  accounts: Account[] | null;
+  selectedAccount: Account | null;
+  onSelectAccount: (category: Account) => void;
+};
+
+export function AccountPicker({ accounts, selectedAccount, onSelectAccount }: Props) {
+  const textStyles = useTextStyles();
+
+  if (!accounts) {
+    return <LoadingIndicator size="small" />;
+  }
+
+  return (
+    <View
+      style={{
+        position: 'relative',
+        width: '100%',
+      }}
+    >
+      <Picker
+        style={textStyles.picker}
+        selectedValue={selectedAccount?.id}
+        onValueChange={(itemValue: string | number) => {
+          const selected = accounts.find((cat) => cat.id === itemValue);
+          if (selected) {
+            onSelectAccount(selected);
+          }
+        }}
+      >
+        {accounts.map((account) => (
+          <Picker.Item key={account.id} label={account.name} value={account.id} style={textStyles.pickerItem} />
+        ))}
+      </Picker>
+    </View>
+  );
+}

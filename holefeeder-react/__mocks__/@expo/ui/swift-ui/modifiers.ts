4ed518ee540f2fed9576c1c9cae10cdede4d4221
Patrick Moreau <patrick@drifterapps.com>
2025-11-21 01:16:06 -0500

    fix(purchase): fix test

--- holefeeder-react/__mocks__/@expo/ui/swift-ui/modifiers.ts
+++ holefeeder-react/__mocks__/@expo/ui/swift-ui/modifiers.ts
@@ -1,3 +1,4 @@
 export const frame = jest.fn((props) => props);
 export const padding = jest.fn((props) => props);
 export const border = jest.fn((props) => props);
+export const font = jest.fn((props) => props);
